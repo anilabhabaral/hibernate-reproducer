ee3b82e7c9281a4f4e207bf84e4d85be8d6753a3
Anilabha Baral <85929177+anilabhabaral@users.noreply.github.com>
2025-02-14 21:07:33 +0530

    Update Main.java

--- test-hibernate-standalone/src/main/java/org/example/Main.java
+++ test-hibernate-standalone/src/main/java/org/example/Main.java
@@ -15,10 +15,10 @@ public class Main {
 	public static void main(String[] args) throws InterruptedException {
 		Main main = new Main();
 
-		System.out.println( Version.getVersionString() );
+		System.out.println("-------> Hibernate Version: "+ Version.getVersionString() );
 
 //		adding a data
-		main.addData();
+//		main.addData();
 //		Thread.sleep( 1000 );
 
 //		main.test2LC();
@@ -26,68 +26,68 @@ public class Main {
 
 	}
 
-	public void test2LC() {
-		entityManagerFactory = Persistence.createEntityManagerFactory( "testPU" );
+	// public void test2LC() {
+	// 	entityManagerFactory = Persistence.createEntityManagerFactory( "testPU" );
 
 
-		// em1 session
-		EntityManager entityManager1 = entityManagerFactory.createEntityManager();
-		entityManager1.getTransaction().begin();
-		Student student1 = entityManager1.find( Student.class, 1 );
-		entityManager1.getTransaction().commit();
-		entityManager1.close();
+	// 	// em1 session
+	// 	EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+	// 	entityManager1.getTransaction().begin();
+	// 	Student student1 = entityManager1.find( Student.class, 1 );
+	// 	entityManager1.getTransaction().commit();
+	// 	entityManager1.close();
 
 
-		// em2 session
-		EntityManager entityManager2 = entityManagerFactory.createEntityManager();
-		entityManager2.getTransaction().begin();
-		Student student2 = entityManager2.find( Student.class, 1 );
-		entityManager2.getTransaction().commit();
-		entityManager2.close();
+	// 	// em2 session
+	// 	EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+	// 	entityManager2.getTransaction().begin();
+	// 	Student student2 = entityManager2.find( Student.class, 1 );
+	// 	entityManager2.getTransaction().commit();
+	// 	entityManager2.close();
 
 
-		entityManagerFactory.close();
+	// 	entityManagerFactory.close();
 
-	}
+	// }
 
-	public void testQueryCache() {
-		entityManagerFactory = Persistence.createEntityManagerFactory( "testPU" );
-
-
-		// em1 session
-		EntityManager entityManager1 = entityManagerFactory.createEntityManager();
-		entityManager1.getTransaction().begin();
-		Query query1 = entityManager1.createQuery( "from Student where id = 1" );
-		query1.setHint( "org.hibernate.cacheable", true );
-		ArrayList<Student> student1 = (ArrayList<Student>) query1.getResultList();
-		entityManager1.getTransaction().commit();
-		entityManager1.close();
+	// public void testQueryCache() {
+	// 	entityManagerFactory = Persistence.createEntityManagerFactory( "testPU" );
 
 
-		// em2 session
-		EntityManager entityManager2 = entityManagerFactory.createEntityManager();
-		entityManager2.getTransaction().begin();
-		Query query2 = entityManager2.createQuery( "from Student where id = 1" );
-		query2.setHint( "org.hibernate.cacheable", true );
-		ArrayList<Student> student2 = (ArrayList<Student>) query2.getResultList();
-		entityManager2.getTransaction().commit();
-		entityManager2.close();
+	// 	// em1 session
+	// 	EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+	// 	entityManager1.getTransaction().begin();
+	// 	Query query1 = entityManager1.createQuery( "from Student where id = 1" );
+	// 	query1.setHint( "org.hibernate.cacheable", true );
+	// 	ArrayList<Student> student1 = (ArrayList<Student>) query1.getResultList();
+	// 	entityManager1.getTransaction().commit();
+	// 	entityManager1.close();
 
 
-		entityManagerFactory.close();
-	}
+	// 	// em2 session
+	// 	EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+	// 	entityManager2.getTransaction().begin();
+	// 	Query query2 = entityManager2.createQuery( "from Student where id = 1" );
+	// 	query2.setHint( "org.hibernate.cacheable", true );
+	// 	ArrayList<Student> student2 = (ArrayList<Student>) query2.getResultList();
+	// 	entityManager2.getTransaction().commit();
+	// 	entityManager2.close();
 
-	public void addData()
-	{
-		entityManagerFactory=Persistence.createEntityManagerFactory( "testPU" );
-		EntityManager entityManager=entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		Student student=new Student();
-		student.setName( "Anilabha" );
-		entityManager.persist( student );
-		entityManager.getTransaction().commit();
-		entityManager.close();
-		entityManagerFactory.close();
-	}
+
+	// 	entityManagerFactory.close();
+	// }
+
+	// public void addData()
+	// {
+	// 	entityManagerFactory=Persistence.createEntityManagerFactory( "testPU" );
+	// 	EntityManager entityManager=entityManagerFactory.createEntityManager();
+	// 	entityManager.getTransaction().begin();
+	// 	Student student=new Student();
+	// 	student.setName( "Anilabha" );
+	// 	entityManager.persist( student );
+	// 	entityManager.getTransaction().commit();
+	// 	entityManager.close();
+	// 	entityManagerFactory.close();
+	// }
 
 }
