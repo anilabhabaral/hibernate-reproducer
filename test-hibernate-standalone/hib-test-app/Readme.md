3353b6024affe4ac4beb7c3662b687d64fe9be33
Anilabha Baral <abaral@redhat.com>
2025-02-14 18:45:49 +0530

    1st commit

---
+++ test-hibernate-standalone/hib-test-app/Readme.md
@@ -0,0 +1,9 @@
+### Hibernate 2LC test application.
+
+Run a MySQL DB using podman/docker:
+```
+podman pull quay.io/rhn_support_abaral1/my-mysql
+```
+```
+podman run -d -p 3306:3306 --name my-mysql -e MYSQL_ROOT_PASSWORD=supersecret my-mysql
+```
---
+++ test-hibernate-standalone/hib-test-app/src/main/java/org/example/Student.java
@@ -0,0 +1,51 @@
+package org.example;
+
+
+import javax.persistence.Cacheable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Entity(name = "Student")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
