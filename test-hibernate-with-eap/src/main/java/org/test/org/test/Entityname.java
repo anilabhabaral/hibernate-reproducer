89f174bcab435afa96a916ea6380fc9e19c585ac
Anilabha Baral <85929177+anilabhabaral@users.noreply.github.com>
2025-02-14 21:05:28 +0530

    Update Entityname.java

--- test-hibernate-with-eap/src/main/java/org/test/org/test/Entityname.java
+++ test-hibernate-with-eap/src/main/java/org/test/org/test/Entityname.java
@@ -1,11 +1,31 @@
 package org.test.org.test;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "TestTable")
 public class Entityname {
 
+	@Id
+	int id;
+	String name;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
